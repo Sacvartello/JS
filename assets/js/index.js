d9e999ac1c7870b299345661f04116a48b2d513f
Sacvertello <georgdvorskii@gmail.com>
2024-07-08 22:24:49 +0300

    Add base of sintasis

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+alert("Hello, world!");
+console.log('Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii');
+let userName = 'bro';
+console.log('userName =', userName);
+
+const conVal =10;
+console.log(conVal);
