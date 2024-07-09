9e29ebd5f77f9bf738dcdeb3c9e8fc8efcfe2349
Sacvertello <georgdvorskii@gmail.com>
2024-07-09 13:13:47 +0300

    Add type of data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,30 @@
 alert("Hello, world!");
-console.log('Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii');
-let userName = 'bro';
-console.log('userName =', userName);
+console.log(
+  "Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
+);
+let userName = "bro";
+console.log("userName =", userName);
 
-const conVal =10;
+const conVal = 10;
 console.log(conVal);
+
+const stVar1 = 'no';
+const stVar2 = "yes";
+const stVar3 = `maybe`;
+console.log(stVar1, stVar2, stVar3);
+
+const numVar = 3.8;
+console.log(numVar)
+
+const boolVal = true
+console.log(boolVal);
+
+const nullVal = null
+console.log(nullVal)
+
+console.log('typeof nullVal:', typeof nullVal)
+
+const myAge = 13;
+const myName = 'Georg';
+console.log(myName, myAge)
+
