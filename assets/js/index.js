e9e885888573dfc6cc16e8ad771b9f9d0eff2bfb
Sacvertello <georgdvorskii@gmail.com>
2024-07-14 11:45:19 +0300

    Add for

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,26 @@
-let sideCount = 1
-const totalSideAmount = 4
-while (sideCount<= totalSideAmount) {
-  console.log('Йти прямо')
-  console.log('Повернути праворуч')
-  sideCount ++
+let sideCount = 1;
+const totalSideAmount = 4;
+while (sideCount <= totalSideAmount) {
+  console.log("Йти прямо");
+  console.log("Повернути праворуч");
+  sideCount++;
 }
 
-let n = null
+let n = null;
 do {
-  n = Number(prompt('Введіть число'))
-} while (!Number.isFinite(n))
-console.log(n*n)
+  n = Number(prompt("Введіть число"));
+} while (!Number.isFinite(n));
+console.log(n * n);
 
-let i = 1
-let t = 1
-while(i<10) {
-  i ++
-  t *= i
+let i = 1;
+let t = 1;
+while (i < 10) {
+  i++;
+  t *= i;
 }
-console.log(t)
+console.log(t);
+
+const TOTAL_PAGE_AMOUNT = 20;
+for (let currentPage = 10; currentPage <= TOTAL_PAGE_AMOUNT; currentPage += 2) {
+  console.log("Page:", currentPage);
+}
