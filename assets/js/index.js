5cfc0ab046697d7704544eaad8752fe20fb7e010
Dgordgun <georgdvorskii@gmail.com>
2024-08-03 10:57:37 +0300

    Practice with try, catch, throw

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,42 @@
-//Рядки
-function getInitials (name){
-  const space = name.indexOf(' ')
-  return (name[0] + name.slice(space + 1, space + 2)).toUpperCase()
-}
-
-const initial = getInitials('vova borovyk')
-console.log(initial);
-//Лексичне оточення. Замикання
-function counter (){
-  let i = 0;
-
-  return function (){
-    return ++i
+//Рекурсія
+function factorial(n){
+  if (n===0){
+    return 1
   }
+  return n * factorial(n-1)
 }
-const counter1 = counter()
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
-const counter2 = counter()
-console.log('counter2() :>> ', counter2());
+const f4 = factorial(4)
+console.log(f4);
+//Обробка помилoк
+
+//try, catch
+try {
+  const a = 8
+  console.log(a);
+  const c = a + b
+  console.log(b);
+} catch (err){
+  console.log('err :>> ', err);
+}
+
+
+console.log('Hello');
+console.log('Hello');
+console.log('Hello');
+console.log('Hello');
+console.log('Hello');
+//Практика
+function realAge (age){
+  if(typeof age !== 'number'){
+    throw new TypeError('age must be a number!')
+  }
+  if (age < 0){
+    throw new RangeError('age must be a positive number!')
+  }
+  return age
+}
+try{
+  console.log(realAge(7));
+} catch (err){
+  console.log(err);
+}
