32903151502286ad544897118df82e2df4b68f74
Dgordgun <georgdvorskii@gmail.com>
2024-08-06 12:10:07 +0300

    Add classes task

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -39,3 +39,32 @@ try{
 } catch (err){
     console.log('err :>> ', err);
 }
+// class Phone{
+//     constructor (brand, model, color, price, year){
+//         this.brand = brand
+//         this.model = model
+//         this.color = color
+//         this.price = price
+//         this.year = year
+//     }
+//     get calcPhoneAge(){
+//         return 2024 - this.year
+//     }
+// }
+// const phone1 = ('Samsung','Model5','white',12000, 2021)
+// console.log(phone1.calcPhoneAge());
+class Phone {
+    constructor(brand, model, color, price, year) {
+      this.brand = brand;
+      this.model = model;
+      this.color = color;
+      this.price = price;
+      this.year = year;
+    }
+    calcPhoneAge() {
+      return new Date().getFullYear() - this.year;
+    }
+  }
+  
+  const phone1 = new Phone('Samsung', 'Model1', 'white', 12000, 2021);
+  console.log(phone1.calcPhoneAge());
