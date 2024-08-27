6ed792105f5c3bef2855b4113a9b652c0cdfde0e
Dgordgun <georgdvorskii@gmail.com>
2024-08-27 14:03:44 +0300

    Add Modal window

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,51 +1,22 @@
-//Task
-const body = document.body
+const showBtn = document.querySelector('#show-modal')
+const closeBtn = document.querySelector('#close-modal')
+const section = document.querySelector('.background')
+const modal = document.querySelector('.modal-window')
 
-const section = document.querySelector('.sec1')
-const article = document.querySelector('.art1')
-const div = document.querySelector('.div1')
-// //1
-// function handler1 (event){
-//     console.log(event.target.localName);
-// }
-
-// div.addEventListener('click', handler1)
-// //2
-// function handler2(event){
-//     console.log('----------------------------');
-//     console.log('target', event.target);
-//     console.log('currentTarget', event.currentTarget);
-// }
-
-// body.addEventListener('click', handler2)
-// section.addEventListener('click', handler2)
-// article.addEventListener('click', handler2)
-// div.addEventListener('click', handler2)
-// //3
-// function handler3(event){
-//     console.log('----------------------------');
-//     console.log('target', event.target);
-//     console.log('currentTarget', event.currentTarget);
-//     if (event.currentTarget===section) {
-//         event.stopPropagation()
-//     }
-// }
-
-// body.addEventListener('click', handler3, true)
-// section.addEventListener('click', handler3, true)
-// article.addEventListener('click', handler3, true)
-// div.addEventListener('click', handler3, true)
-//4
-function handler4(event){
-    if (event.currentTarget=== event.target) {
-        event.stopPropagation()
-    }
-    console.log('----------------------------');
-    console.log('target', event.target);
-    console.log('currentTarget', event.currentTarget);
+function showModal(){
+    section.style.display = 'flex'
+    modal.style.display = 'block'
 }
 
-body.addEventListener('click', handler4)
-section.addEventListener('click', handler4)
-article.addEventListener('click', handler4)
-div.addEventListener('click', handler4)
+function closeModal(){
+    section.style.display = 'none'
+    modal.style.display = 'none'
+}
+showBtn.addEventListener('click',showModal)
+closeBtn.addEventListener('click',closeModal)
+section.addEventListener('click',closeHandler)
+function closeHandler(event){
+    if (event.target == event.currentTarget) {
+        closeModal()
+    }
+}
