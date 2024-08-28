caa5e2f345e6df9757cdec9576ea22132d0bca55
Dgordgun <georgdvorskii@gmail.com>
2024-08-28 18:07:24 +0300

    Add game coordinate

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -2,6 +2,13 @@
 const section = document.querySelector('section')
 const article = document.querySelector('article')
 function count(event){
-    article.textContent = `X: ${event.x} Y: ${event.y}`
-}
+    if (event.target === event.currentTarget) {
+        const div = event.target.children.box
+        article.textContent = `X: ${event.x} Y: ${event.y}`
+        const w = event.x - (div.offsetWidth/2)
+        const h = event.y - (div.offsetHeight /2)
+        div.style.top = `${h}px`
+        div.style.left = `${w}px`
+    }
+}   
 section.addEventListener('click', count)
