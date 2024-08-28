f1c90a5dc1c04a6eef09eea71624572182f4ca67
Dgordgun <georgdvorskii@gmail.com>
2024-08-28 18:38:46 +0300

    Rework game

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,14 +1,53 @@
-//Task
 const section = document.querySelector('section')
 const article = document.querySelector('article')
+class Game {
+    constructor(scoreElement) {
+       this.count = 0;
+        this.scoreElement = scoreElement
+    }
+
+    get count() {
+        return this._count
+    }
+
+    set count(v) {
+        this._count = v;
+    }
+
+    increment() {
+        ++this.count;
+        this.rerender();
+        return this.count;
+    }
+
+    decrement() {
+        --this.count;
+        this.rerender();
+        return this.count;
+    }
+
+    rerender(){
+        this.scoreElement.textContent = `Score: ${this.count}`;
+    }
+}
+
+//Task
+const game = new Game(article)
 function count(event){
-    if (event.target === event.currentTarget) {
-        const div = event.target.children.box
-        article.textContent = `X: ${event.x} Y: ${event.y}`
-        const w = event.x - (div.offsetWidth/2)
-        const h = event.y - (div.offsetHeight /2)
+    if(event.target === event.currentTarget) {
+        game.decrement(); 
+    } else {
+        game.increment();
+    }
+
+        const div = event.currentTarget.children.box
+        const w = getRandom(0, event.currentTarget.offsetWidth - (div.offsetWidth/2))
+        const h = getRandom(0, event.currentTarget.offsetHeight - (div.offsetHeight/2))
         div.style.top = `${h}px`
         div.style.left = `${w}px`
-    }
 }   
 section.addEventListener('click', count)
+
+function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1))+ min
+}
