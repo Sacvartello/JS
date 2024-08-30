f8c631b59bd5908895d379dd4a65e04e672e25ed
Dgordgun <georgdvorskii@gmail.com>
2024-08-30 10:22:55 +0300

    Create attributes

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,32 +1,11 @@
-function handler1(event){
-    const STEP = 10
-    const box = event.currentTarget.body.children.box
-    const x = box.offsetLeft
-    const y = box.offsetTop
-    console.dir();
-    switch (event.code) {
-        case 'ArrowUp':{
-            box.style.top = `${y - STEP}px`
-            break;
-        }
-        
-        case 'ArrowDown':{
-            box.style.top = `${y + STEP}px`
-            break;
-        }
+const addressImgURL = 'https://images.pexels.com/photos/27245504/pexels-photo-27245504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
-        case 'ArrowLeft':{
-            box.style.left = `${x - STEP}px`
-            break;
-        }
-        case 'ArrowRight':{
-            box.style.left = `${x + STEP}px`
-            break;
-        }
+const img = document.querySelector('.img')
+const attrSrc = document.createAttribute('src')
+img.setAttributeNode(attrSrc)
+attrSrc.value = addressImgURL
 
-        default: {
-            break;
-        }
-    }
+function changeImg(attrSrc){
+    const newImg = 'https://images.pexels.com/photos/27576423/pexels-photo-27576423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    attrSrc.value = newImg
 }
-document.addEventListener('keydown', handler1)
