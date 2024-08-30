1aa476befba18ae14dd8d01f33bee071461a9623
Dgordgun <georgdvorskii@gmail.com>
2024-08-30 17:43:10 +0300

    Add side-menu

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,21 +1,12 @@
-const btn = document.querySelector('#btn')
-function newElement() {
-    const newEl = document.createElement('div')
-    const body = document.body
-    newEl.className = 'red-circle'
-    body.append(newEl)
-    // newEl.style.width = '80px'
-    // newEl.style.height = '80px'
-    // newEl.style.backgroundColor = 'red'
-    // newEl.style.borderRadius = '50%'
+const menuContainer = document.querySelector('.menu-container')
 
-    function makeGreen(event){
-        const cl = event.target.classList
-        cl.toggle('green-square')
-        // newEl.style.backgroundColor='green'
-        // newEl.style.borderRadius = '0'
-    }
-
-    newEl.addEventListener('click', makeGreen)
+function change(event){
+    event.stopPropagation()
+    event.currentTarget.classList.toggle('change')
+    showMenu(event)
 }
-btn.addEventListener('click', newElement)
+function showMenu(event){
+    const sideMenu = document.querySelector('.side-menu')
+    sideMenu.classList.toggle('show')
+}
+menuContainer.addEventListener('click', change, true)
