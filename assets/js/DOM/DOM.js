c2c9db9c2bf0102fba00c7928b827890ca7833b6
Dgordgun <georgdvorskii@gmail.com>
2024-08-30 16:50:25 +0300

    Rework red circle task

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -2,15 +2,18 @@ const btn = document.querySelector('#btn')
 function newElement() {
     const newEl = document.createElement('div')
     const body = document.body
+    newEl.className = 'red-circle'
     body.append(newEl)
-    newEl.style.width = '80px'
-    newEl.style.height = '80px'
-    newEl.style.backgroundColor = 'red'
-    newEl.style.borderRadius = '50%'
+    // newEl.style.width = '80px'
+    // newEl.style.height = '80px'
+    // newEl.style.backgroundColor = 'red'
+    // newEl.style.borderRadius = '50%'
 
-    function makeGreen(){
-        newEl.style.backgroundColor='green'
-        newEl.style.borderRadius = '0'
+    function makeGreen(event){
+        const cl = event.target.classList
+        cl.toggle('green-square')
+        // newEl.style.backgroundColor='green'
+        // newEl.style.borderRadius = '0'
     }
 
     newEl.addEventListener('click', makeGreen)
