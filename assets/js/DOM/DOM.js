0dfe6ec41b1b6385dba23c7a8405fd6ea6bcf1da
Dgordgun <georgdvorskii@gmail.com>
2024-08-30 16:15:57 +0300

    Add red circle task

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,37 +1,18 @@
-const imgs = ['https://images.pexels.com/photos/27245504/pexels-photo-27245504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-'https://images.pexels.com/photos/27576423/pexels-photo-27576423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-'https://images.pexels.com/photos/27926212/pexels-photo-27926212.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-'https://images.pexels.com/photos/27637648/pexels-photo-27637648.jpeg',
-'https://images.pexels.com/photos/27935498/pexels-photo-27935498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-'https://images.pexels.com/photos/16541441/pexels-photo-16541441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-'https://images.pexels.com/photos/27849805/pexels-photo-27849805.png'
-]
+const btn = document.querySelector('#btn')
+function newElement() {
+    const newEl = document.createElement('div')
+    const body = document.body
+    body.append(newEl)
+    newEl.style.width = '80px'
+    newEl.style.height = '80px'
+    newEl.style.backgroundColor = 'red'
+    newEl.style.borderRadius = '50%'
 
-const img = document.querySelector('.img')
-const attrSrc = document.createAttribute('src')
-const [prevBtn,nextBtn] = document.querySelectorAll('.btn')
-img.setAttributeNode(attrSrc)
-
-const slider = new Slider(imgs)
-
-function updateView() {
-    attrSrc.value= slider.currentSlide
-}
-function createBtnHandler(direction){
-    return function () {
-        slider.currentIndex = slider[direction]
-        updateView()
+    function makeGreen(){
+        newEl.style.backgroundColor='green'
+        newEl.style.borderRadius = '0'
     }
-}
 
-// function prevImg() {
-//     slider.currentIndex = slider.prev()
-//     updateView()
-// }
-// function nextImg() {
-//     slider.currentIndex = slider.next()
-//     updateView()
-// }
-updateView()
-prevBtn.addEventListener('click', createBtnHandler('prev'))
-nextBtn.addEventListener('click', createBtnHandler('next'))
+    newEl.addEventListener('click', makeGreen)
+}
+btn.addEventListener('click', newElement)
