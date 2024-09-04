47e680d4f7d2b725ee9513178f3e0e35d11b44a8
Dgordgun <georgdvorskii@gmail.com>
2024-09-04 19:55:42 +0300

    Add show-hide pass

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,11 +1,63 @@
-const form = document.querySelector('form')
+const form = document.querySelector('form');
+const inputFirstName = document.querySelector('input[name="firstName"]');
 
-function handler1(event){
-    event.preventDefault()
-    const form = event.target
-    const inputUserName= form.user.value
-    console.log(inputUserName);
+
+inputFirstName.addEventListener('change', function({target}) {
+
+    if(validateName(target.value)){
+        target.classList.remove('invalid');
+       target.classList.add('valid')
+    } else {
+        target.classList.add('invalid')
+    }
+})
+
+
+function validateName(value) {
+    return !(value.includes('@') || value.includes('!')) 
 }
 
-form/addEventListener('submit', handler1)
+form.addEventListener('submit', function(event){
+    // потрібно зупинити поведінку за замовчуванням!
+    event.preventDefault(); // виклик методу зупиняє поведінку за-замовчуванням
+    const form = event.target;
+    if (validateData(form.pass.value, form.passRepeat.value)) {
+        submitDataToServer(form);
+    } else {
+        createErrorMessage(form)
+    }
+})
 
+function validateData(inputValue1, inputValue2) {
+    return inputValue1 === inputValue2
+}
+
+
+function submitDataToServer({firstName, lastName, email, pass, passRepeat, birthday, agreement}){
+    const userObj = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        password: pass.value,
+        birthday: birthday.value,
+        agree: agreement.checked
+    }
+       console.log(userObj); // відправляємо дані на сервер
+}
+function createErrorMessage(form) {
+    const errorMessage = document.createElement('p');
+    errorMessage.classList.add('error-text');
+    errorMessage.textContent = 'Password must be the same';
+    form.append(errorMessage);
+}
+
+const icon = document.querySelector('.pass-icon')
+const passInput = document.querySelector('input[name="pass"]')
+
+icon.addEventListener('click', function () {
+    if(passInput.type === 'password'){
+        passInput.type = 'text'
+    } else{
+        passInput.type = 'password'
+    }
+})
