7f79bb79ac4df0d9c7d6b4a01bd508772ef1f2f1
Dgordgun <georgdvorskii@gmail.com>
2024-09-08 19:49:28 +0300

    ToDo list

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,63 +1,25 @@
-const form = document.querySelector('form');
-const inputFirstName = document.querySelector('input[name="firstName"]');
-
-
-inputFirstName.addEventListener('change', function({target}) {
-
-    if(validateName(target.value)){
-        target.classList.remove('invalid');
-       target.classList.add('valid')
-    } else {
-        target.classList.add('invalid')
-    }
-})
-
-
-function validateName(value) {
-    return !(value.includes('@') || value.includes('!')) 
+const form = document.querySelector('form')
+const ol = document.querySelector('ol')
+const arr1 = []
+function handler1(event){
+    event.preventDefault()
+    const form = event.target
+    const inputUserName= form.user.value
+    checkText(inputUserName)
 }
-
-form.addEventListener('submit', function(event){
-    // потрібно зупинити поведінку за замовчуванням!
-    event.preventDefault(); // виклик методу зупиняє поведінку за-замовчуванням
-    const form = event.target;
-    if (validateData(form.pass.value, form.passRepeat.value)) {
-        submitDataToServer(form);
-    } else {
-        createErrorMessage(form)
-    }
-})
-
-function validateData(inputValue1, inputValue2) {
-    return inputValue1 === inputValue2
-}
-
-
-function submitDataToServer({firstName, lastName, email, pass, passRepeat, birthday, agreement}){
-    const userObj = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        password: pass.value,
-        birthday: birthday.value,
-        agree: agreement.checked
-    }
-       console.log(userObj); // відправляємо дані на сервер
-}
-function createErrorMessage(form) {
-    const errorMessage = document.createElement('p');
-    errorMessage.classList.add('error-text');
-    errorMessage.textContent = 'Password must be the same';
-    form.append(errorMessage);
-}
-
-const icon = document.querySelector('.pass-icon')
-const passInput = document.querySelector('input[name="pass"]')
-
-icon.addEventListener('click', function () {
-    if(passInput.type === 'password'){
-        passInput.type = 'text'
+function checkText(value) {
+    if(value===''){
+        console.log('err');
     } else{
-        passInput.type = 'password'
+        addToDoc(value, arr1)
     }
-})
+}
+function addToDoc(value, arr){
+    arr.push(value)
+    const li = document.createElement('li')
+    li.textContent = value
+    ol.className ='createOl'
+    ol.append(li)
+    console.log(arr);
+}
+form.addEventListener('submit', handler1)
