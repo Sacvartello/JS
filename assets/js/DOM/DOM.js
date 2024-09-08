4447ee4466bc5da3a2694fa7d5155f5f4178eeaa
Dgordgun <georgdvorskii@gmail.com>
2024-09-08 19:58:12 +0300

    Add JSON

--- assets/js/DOM/DOM.js
+++ assets/js/DOM/DOM.js
@@ -1,25 +1,8 @@
-const form = document.querySelector('form')
-const ol = document.querySelector('ol')
-const arr1 = []
-function handler1(event){
-    event.preventDefault()
-    const form = event.target
-    const inputUserName= form.user.value
-    checkText(inputUserName)
+//JSON
+const obj = {
+    n:'g',
+    g:111
 }
-function checkText(value) {
-    if(value===''){
-        console.log('err');
-    } else{
-        addToDoc(value, arr1)
-    }
-}
-function addToDoc(value, arr){
-    arr.push(value)
-    const li = document.createElement('li')
-    li.textContent = value
-    ol.className ='createOl'
-    ol.append(li)
-    console.log(arr);
-}
-form.addEventListener('submit', handler1)
+const str = JSON.stringify(obj)
+console.log(str);
+console.log(JSON.parse(str));
